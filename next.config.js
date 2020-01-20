bfdc56a76a2df082e00e213464d0651d4c79c86e
han-tyumi <mmchamp95@gmail.com>
2020-01-20 17:55:21 -0500

    Add dotenv

---
+++ next.config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
+
+module.exports = {
+  env: {
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET
+  }
+};
