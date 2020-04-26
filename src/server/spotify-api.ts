f80a2804950f6699057823648bf79e2ea1b35666
han-tyumi <mmchamp95@gmail.com>
2020-04-26 01:22:53 -0400

    Remove Spotify production check

--- src/server/spotify-api.ts
+++ src/server/spotify-api.ts
@@ -3,10 +3,7 @@ import SpotifyWebApi from 'spotify-web-api-node';
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
   clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  redirectUri:
-    process.env.NODE_ENV === 'production'
-      ? process.env.SPOTIFY_REDIRECT_URI
-      : 'https://localhost:3000/api/callback',
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
 });
 
 export default spotifyApi;
